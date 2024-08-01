13bd438c8f43a29305a620b9ba27a1e24384bc69
kbleul <kbleulseged@gmail.com>
2024-08-01 06:06:18 +0300

    DONE - BUG FIX - navigate after logout error

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { routes } from "@/config/routes";
 import { useIsMounted } from "@/hooks/use-is-mounted";
 import MainLayout from "@/layouts/layout";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function DefaultLayout({
   children,
@@ -10,12 +12,17 @@ export default function DefaultLayout({
 }) {
   const isMounted = useIsMounted();
   const { data: session } = useSession();
+  const router = useRouter();
 
   if (!isMounted) {
     return null;
   }
 
   console.log(session);
+
+  if (!session) {
+    router.push(routes.signIn);
+  }
 
   return <MainLayout>{children}</MainLayout>;
 }
